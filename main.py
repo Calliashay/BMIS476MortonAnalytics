1c4fcfc4ac218980cb548d3c73b90acbce1eac4b
Matt True <matt.true@umontana.edu>
2026-02-25 13:40:58 -0700

    File Readers and Morton Import.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import polars as pl
 import numpy as np
 from adbc_driver_postgresql import dbapi
-
+import morton_Data_01_01
 
 
 ## load vessel finder feed data
--- morton_Data_01_01.py
+++ morton_Data_01_01.py
@@ -5,7 +5,7 @@ def load_dataset(file_path):
         df = pd.read_csv(file_path)
         print("Dataset loaded successfully!\n")
         print("First 3 rows:")
-        print(df.head())
+        print(df.head(3))
         return df
     except FileNotFoundError:
         print("Error: File not found.")
@@ -13,5 +13,16 @@ def load_dataset(file_path):
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    file_name = "AIS_2024_01_01.csv"  # <-- 
-    data = load_dataset(file_name)
+    file_name = "AIS_2024Updated (1).csv"
+    
+    # Load dataset
+    df = load_dataset(file_name)
+
+    if df is not None:
+        # Take random 50,000 row sample
+        sample = df.sample(n=50000, random_state=42)
+        
+        # Save sample
+        sample.to_csv("ais_sample_50k.csv", index=False)
+        
+        print("\nRandom 50,000 row sample saved as 'ais_sample_50k.csv'")
